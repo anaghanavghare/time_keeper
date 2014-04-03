d35140db2991bf45a93093859960e3091b57847a
Anagha <anagha@idyllic-software.com>
2014-04-03 12:22:38 +0530

    Removed unwanted code.

--- app/controllers/welcome_controller.rb
+++
@@ -1,8 +0,0 @@
-class WelcomeController < ActionController::Base
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
-  before_filter :authenticate_user!
-  def index
-  	
-  end
-end
