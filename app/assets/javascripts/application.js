a2c6b257386d28c71a3ec89626d951fb2d1b7b9a
vinu123 <wenothe@gmail.com>
2014-04-03 13:33:40 +0530

    Added angular and used application layout for logged in template

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -9,4 +9,5 @@
 //
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
-//
+//= require angular.min
+//= require angular-route.min
