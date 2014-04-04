fe9e6b9e4066c3884dffa4971f3b9cc0f5d5dd4b
Anagha <anagha@idyllic-software.com>
2014-04-04 16:09:50 +0530

    Added first_name last_name.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140404060523) do
+ActiveRecord::Schema.define(version: 20140404103834) do
 
   create_table "projects", force: true do |t|
     t.string   "name"
@@ -36,6 +36,8 @@ ActiveRecord::Schema.define(version: 20140404060523) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "roles_mask"
+    t.string   "first_name"
+    t.string   "last_name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
