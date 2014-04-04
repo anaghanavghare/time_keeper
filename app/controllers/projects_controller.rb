8abe5aae7a84237eaa9aa3ffffba7ba67c937432
Anagha <anagha@idyllic-software.com>
2014-04-04 12:28:28 +0530

    Added edit, delete and index function for projects.

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -3,7 +3,7 @@ class ProjectsController < ApplicationController
   # For APIs, you may want to use :null_session instead.
   before_filter :authenticate_user!
   def index
-  	
+  	@projects = Project.all
   end
 
   # Required field are name, timeframe
@@ -18,6 +18,15 @@ class ProjectsController < ApplicationController
   	end
   end
 
+  def edit
+    @project = Project.find(params[:id])
+    if @project
+      render :json => @project, :status => :ok
+    else
+      render :json => "Project not found", :status => :error
+    end
+  end
+
   def update
     @project = Project.find(params[:id])
     if @project.update_attributes(params[:project])
@@ -26,6 +35,15 @@ class ProjectsController < ApplicationController
     else
       response_hash = {:errors => @project.errors.full_messages}
       render :json => response_hash, :status => :error
+    end
+  end
+
+  def destroy
+    @project = Project.find(params[:id])
+    if @project and @project.destroy
+      render :json => "Project was delted succesfully.", :status => :ok
+    else
+      render :json => "Project not found", :status => :error
     end
   end
 end
