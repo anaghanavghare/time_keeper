e82b65ce0504eb58fbda0729f7b6cb5a53f38874
Anagha <anagha@idyllic-software.com>
2014-04-04 12:25:48 +0530

    Added edit, delete and index function for users.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,8 @@ class UsersController < ApplicationController
   before_filter :authenticate_user!
   #layout 'application'
   def index
-  	
+  	@users = User.all.reject { |u|  u.id == 1}
+    render :json => @users, :status => :ok
   end
 
   def create
@@ -19,6 +20,15 @@ class UsersController < ApplicationController
   	end
   end
 
+  def edit
+    @user = User.find(params[:id])
+    if @user
+      render :json => @user, :status => :ok
+    else
+      render :json => "User not found", :status => :error
+    end
+  end
+
   def update
     @user = User.find(params[:id])
     @user.update_role(params[:user][:role])
@@ -28,6 +38,15 @@ class UsersController < ApplicationController
     else
       response_hash = {:errors => @user.errors.full_messages}
       render :json => response_hash, :status => :error
+    end
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    if @user and @user.destroy
+      render :json => "User was delted succesfully.", :status => :ok
+    else
+      render :json => "User not found", :status => :error
     end
   end
 end
