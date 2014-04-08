dbeb57890d14102332ee89dcac90d0364bb2413b
vinu123 <wenothe@gmail.com>
2014-04-08 12:09:29 +0530

    Added all members partial

--- app/assets/javascripts/membersController.js
+++ app/assets/javascripts/membersController.js
@@ -1,13 +1,20 @@
 timeKeeperApp.controller('membersController', ['$scope', '$http', function ($scope, $http){
+    // Form values object
     $scope.formData = {
         user: {}
     };
+    // Sending the form values to the rails controller
     $scope.processForm = function(isValid) {
         if (isValid) {
-            $http.post('/members.json', angular.toJson($scope.formData))
+            $http.post('/members.json', $scope.formData)
             .success(function(data) {
                 console.log(data);
             });
         }
     };
+
+    // All members
+    $http.get('/members.json').success(function(data) {
+        $scope.members = data;
+    });
 }]);
--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -2,7 +2,7 @@ class MembersController < ApplicationController
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   before_filter :authenticate_user!
-  load_and_authorize_resource :user, :parent => false
+  #load_and_authorize_resource :user, :parent => false
   #layout 'application'
   def index
   	@users = User.all.reject { |u|  u.id == 1}
